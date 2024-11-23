70eef98fbe96555cbba34a46cb0ef0016b9faedf
ChoiAnYong <dksdyd78@naver.com>
2024-11-24 06:41:58 +0900

    [Feat] #4 - SplashView 구현

--- 35th-SOPKATHON-Team3-iOS/35th-SOPKATHON-Team3-iOS/Presentation/Splash/SplashViewController.swift
+++ 35th-SOPKATHON-Team3-iOS/35th-SOPKATHON-Team3-iOS/Presentation/Splash/SplashViewController.swift
@@ -5,3 +5,48 @@
 //  Created by 조혜린 on 11/23/24.
 //
 
+import UIKit
+
+import SnapKit
+import Then
+
+final class SplashViewController: UIViewController {
+    private let logoImageView = UIImageView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setStyle()
+        setUI()
+        setLaout()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        moveToOnboarding()
+    }
+    
+    private func setStyle() {
+        view.backgroundColor = .white
+        
+        logoImageView.do {
+            $0.image = .logo
+        }
+    }
+    
+    private func setUI() {
+        view.addSubview(logoImageView)
+    }
+    
+    private func setLaout() {
+        logoImageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    private func moveToOnboarding() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
+            let onboardingViewController = OnboardingViewController()
+            self.navigationController?.pushViewController(onboardingViewController, animated: true)
+        }
+    }
+}
